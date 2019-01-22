9b485ec19752e177642cd1830a5553e7a3bb7b96
Rachel Wyatt <rw126@evansville.edu>
2019-01-22 20:43:17 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,10 @@
 class SongsController < ApplicationController
-
+ 
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+  
   post '/songs' do
     @song = Song.create(:name => params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
@@ -7,11 +12,5 @@ class SongsController < ApplicationController
     @song.save
     redirect to("/songs/#{@song.slug}")
   end
-
-  get '/songs' do
-    @songs = Song.all
-    erb :'songs/index'
-  end
-
 
 end

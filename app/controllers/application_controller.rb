df809cf86918ae4d6c68f0e1ec3df126854c0287
Rachel Wyatt <rw126@evansville.edu>
2019-01-22 20:42:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,6 @@ class ApplicationController < Sinatra::Base
 
 
 
-  post '/songs' do
-    @song = Song.create(:name => params["Name"])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-    @song.genre_ids = params[:genres]
-    @song.save
-    redirect to("/songs/#{@song.slug}")
-  end
-
-  get '/songs' do
-    @songs = Song.all
-    erb :'songs/index'
-  end
 
   get '/artists' do
     @artists = Artist.all
